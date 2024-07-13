8ad9c1daef0987127aa4c9e12f70b0ca1c123a06
Alcenirdev08 <88070760+Alcenirdev08@users.noreply.github.com>
2024-07-13 17:43:00 -0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,10 +1,54 @@
 class Queue {
-  elements
-  constructor(){
-    this.elements = []
+  constructor() {
+    this.elements = [];
+    this.queueSize = 0;
   }
-  
-  // Implementar aqui
+
+  size() {
+    return this.queueSize;
+  }
+
+  add(item) {
+    this.elements.push(item);
+    this.queueSize++;
+  }
+
+  peek() {
+    return this.elements[0];
+  }
+
+  dequeue() {
+    this.queueSize--;
+    return this.elements.shift();
+  }
 }
 
-module.exports = Queue
+module.exports = Queue;
+describe('Queue', () => {
+  let queue;
+},
+
+
+  it('Deve criar uma fila', () => {
+    const queue = new Queue();
+
+    expect(queue).toEqual({ elements: [] });
+  }),
+
+  it('Deve ser capaz de calcular o tamanho da fila', () => {
+    const queue = new Queue();
+    expect(queue.size()).toEqual(1);
+  }),
+
+  it('Deve ser capaz de adicionar um item na fila', () => {
+    queue.add(1);
+    expect(queue.size()).toEqual(1);
+  }),
+
+
+  it('Deve escolher o primeiro item da fila', () => {
+    queue.add(2);
+    queue.add(3);
+    expect(queue.peek()).toEqual(1);
+    expect(queue.size()).toEqual(3);
+  }));
